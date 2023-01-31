51c1edf8e9308518725ceb3917f02944ae29f86e
Gustavo Neri <guganeriss@gmail.com>
2023-01-31 22:57:37 +0000

    Example another test

--- Curso GOLANG/testes/arquitetura/arquitetura_test.go
+++ Curso GOLANG/testes/arquitetura/arquitetura_test.go
@@ -1,9 +1,12 @@
 package arquitetura
 
 func TestDependente(t *testing.T) {
+	t.Parallel()
 	if runtime.GOARCH == "amd64" {
 		t.Skip("Não funciona em arquitetura amd64")
 	}
 	//...
 	t.Fail()
+
+	// go test ./...
 }
