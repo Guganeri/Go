c251f1887b0f1af298d30c8b1fe291814b4a8f96
Guga Neri <guganeriss@gmail.com>
2023-07-04 17:43:38 +0100

    aula 04

--- Aprenda GO/GO101/main.go
+++ Aprenda GO/GO101/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 var (
 	nome string
@@ -16,7 +19,20 @@ func sum(a, b int) int {
 	return a + b
 }
 
+func convertAndSum(a int, b string) (total int, err error) {
+	i, err := strconv.Atoi(b)
+	if err != nil {
+		return
+	}
+
+	total = a + i
+	return
+
+}
+
 func main() {
 	hello("Android")
 	fmt.Println("Total:", sum(10, 5))
+	total, err := convertAndSum(10, "23")
+	fmt.Println("total:", total, err)
 }
