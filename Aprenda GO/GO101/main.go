298e8bc802d651965fbca9be7f200ac732a4dce8
Guga Neri <guganeriss@gmail.com>
2023-07-04 17:31:18 +0100

    aula 04

--- Aprenda GO/GO101/main.go
+++ Aprenda GO/GO101/main.go
@@ -8,22 +8,15 @@ var (
 	n2   int32
 )
 
-func main() {
-
-	var a, b, c = true, 2.3, "Olá"
-	println(a, b, c)
-
-	mensagem := "Aula 03"
-	fmt.Println(mensagem)
-
-	var total int
-	total++
-	fmt.Println("total:", total)
-	nome = "Teste temp"
+func hello(nome string) {
 	fmt.Println("Hello", nome, "!")
+}
 
-	var x, y = 10, 20
-	fmt.Println(x, y)
-	x, y = y, x
-	fmt.Println(x, y)
+func sum(a, b int) int {
+	return a + b
+}
+
+func main() {
+	hello("Android")
+	fmt.Println("Total:", sum(10, 5))
 }
