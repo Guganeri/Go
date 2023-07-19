04821d28addcf36d982eb1a06eb1e18f72ff9487
Guga Neri <guganeriss@gmail.com>
2023-07-19 14:07:26 +0100

    update main

--- Full-Cycle/main.go
+++ Full-Cycle/main.go
@@ -2,33 +2,13 @@ package main
 
 import "github.com/guganeri/Go/Full-Cycle/internal/entity"
 
-type Car struct {
-	Model string
-	Color string
-}
-
-// metodo
-func (c Car) Start() {
-	println(c.Model + " has been is started")
-}
-
-func (c *Car) ChangeColor(color string) {
-	c.Color = color
-	println("New color: " + c.Color)
-}
-
 func main() {
 
-	order := entity.NewOrder("1", 10, 1)
-
-	car := Car{ //declarando e atribuindo a variável
-		Model: "Ferrari",
-		Color: "Red",
+	order, err := entity.NewOrder("1", 10, 1)
+	if err != nil {
+		println(err.Error())
+	} else {
+		println(order.ID)
 	}
-	//car.Model = "Fiat"
-	//println(car.Model)
-	car.Start()
-	car.ChangeColor("Blue")
 
-	println(car.Color)
 }
