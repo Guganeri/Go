e6cbe43e9e155e9f6d4382008d5abdf8adbb12df
Guga Neri <guganeriss@gmail.com>
2023-07-27 20:03:50 +0100

    create cmd

---
+++ Full-Cycle/cmd/order/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/guganeri/Go/Full-Cycle/internal/infra/database"
+	"github.com/guganeri/Go/Full-Cycle/internal/usecase"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+
+	input := usecase.OrderInput{
+		ID:    "123",
+		Price: 10.0,
+		Tax:   1.0,
+	}
+	output, err := uc.Execute(input)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(output)
+}
--- Full-Cycle/main.go
+++ Full-Cycle/main.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-
-	"github.com/guganeri/Go/Full-Cycle/internal/infra/database"
-	"github.com/guganeri/Go/Full-Cycle/internal/usecase"
-	_ "github.com/mattn/go-sqlite3"
+	"time"
 )
 
+func processando() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Second)
+	}
+}
+
+// T1
 func main() {
-	db, err := sql.Open("sqlite3", "db.sqlite3")
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	orderRepository := database.NewOrderRepository(db)
-	uc := usecase.NewCalculateFinalPrice(orderRepository)
-
-	input := usecase.OrderInput{
-		ID:    "123",
-		Price: 10.0,
-		Tax:   1.0,
-	}
-	output, err := uc.Execute(input)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(output)
+	go processando() // T2
+	go processando() // T3
+	processando()    // T1
 }
