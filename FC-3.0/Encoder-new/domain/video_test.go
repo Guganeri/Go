e0ae236c2119b4edd94837ac29be9d29b529a176
Guga Neri <guganeriss@gmail.com>
2023-08-17 19:37:33 +0100

    validate test video

--- FC-3.0/Encoder-new/domain/video_test.go
+++ FC-3.0/Encoder-new/domain/video_test.go
@@ -2,8 +2,10 @@ package domain_test
 
 import (
 	"testing"
+	"time"
 
 	"FC-3.0/Encoder-new/domain"
+	"github.com/docker/distribution/uuid"
 	"github.com/stretchr/testify/require"
 )
 
@@ -12,4 +14,28 @@ func TestValidateIfVideoIsEmpty(t *testing.T) {
 	err := video.Validate()
 
 	require.Error(t, err)
+}
+
+func TestVideoIdIsNotAUuid(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = "abc"
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Error(t, err)
+}
+
+func TestVideoValidation(t *testing.T) {
+	video := domain.NewVideo()
+
+	video.ID = uuid.Generate().String()
+	video.ResourceID = "a"
+	video.FilePath = "path"
+	video.CreatedAt = time.Now()
+
+	err := video.Validate()
+	require.Nil(t, err)
 }
