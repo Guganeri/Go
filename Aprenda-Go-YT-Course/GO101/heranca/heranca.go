09138cfbf9737a79558dbbc16dbca9e7181f1248
Guga Neri <guganeriss@gmail.com>
2023-08-27 17:55:58 +0100

    outro exemplo assercoes

--- Aprenda-Go-YT-Course/GO101/heranca/heranca.go
+++ Aprenda-Go-YT-Course/GO101/heranca/heranca.go
@@ -36,9 +36,15 @@ func (pj PessoaJuridica) Doc() string {
 }
 
 func show(d Document) {
-	if d, ok := d.(PessoaFisica); ok {
-		fmt.Println(d.Sobrenome)
+	switch d.(type) {
+	case PessoaFisica:
+		fmt.Println(d.(PessoaFisica).Sobrenome)
+	case PessoaJuridica:
+		fmt.Println(d.(PessoaJuridica).RazaoSocial)
+	default:
+		fmt.Println("tipo desconhecido")
 	}
+
 	fmt.Println(d)
 	fmt.Println(d.Doc())
 }
@@ -66,7 +72,7 @@ func main() {
 		"temporin",
 		"000.000.000.00",
 	}
-	fmt.Println(p)
+	//fmt.Println(p)
 
 	show(p)
 
