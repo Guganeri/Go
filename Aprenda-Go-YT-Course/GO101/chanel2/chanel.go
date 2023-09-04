05d406a2290ccd2a8b9e3c9d05a78a0617593533
Guga Neri <guganeriss@gmail.com>
2023-09-04 23:54:17 +0100

    Aula16

--- Aprenda-Go-YT-Course/GO101/chanel2/chanel.go
+++ Aprenda-Go-YT-Course/GO101/chanel2/chanel.go
@@ -11,16 +11,17 @@ func numeros(n chan<- int) {
 		fmt.Printf("Escrito no chanel: %d\n", i)
 		time.Sleep(time.Millisecond * 150)
 	}
+	fmt.Println("Fim da escrita")
 	close(n)
 }
 
 func main() {
-	cn := make(chan int)
+	cn := make(chan int, 3)
 	go numeros(cn)
 
 	for v := range cn {
 		fmt.Printf("lido do chanel: %d\n", v)
-		time.Sleep(time.Millisecond * 130)
+		time.Sleep(time.Millisecond * 300)
 	}
 	fmt.Println("Fim da execução")
 }
